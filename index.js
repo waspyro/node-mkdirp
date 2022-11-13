56e060bef3578ec9d66320707ee9edb14c80d39d
Dmitry <ximoske@gmail.com>
2022-11-13 18:07:51 +0300

    🤔

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {dirname} from "path";
 
-export const mkdirp = (dir, cb) =>
+export default const mkdirp = (dir, cb) =>
   fs.stat(dir, (err, stats) => {
     if (!err) stats.isDirectory()
       ? cb(null, dir)
@@ -12,4 +12,4 @@ export const mkdirp = (dir, cb) =>
         : fs.mkdir(dir, cb))
   })
 
-export const mkdirpromise = (dir) => new Promise((rs, rj) => mkdirp(dir, (e, d) => e ? rj(e) : rs(d)))
+export const mkdirP = (dir) => new Promise((rs, rj) => mkdirp(dir, (e, d) => e ? rj(e) : rs(d)))
